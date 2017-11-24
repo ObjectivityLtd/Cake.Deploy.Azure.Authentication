34ad97cd3d2ce3a57199466c2466cdf052e94821
mkiedrowski <mkiedrowski@objectivity.co.uk>
2017-11-24 15:34:10 +0100

    Add method for login using service principal credentials

--- src/Aliases.cs
+++ src/Aliases.cs
@@ -1,12 +1,13 @@
 namespace Cake.Deploy.Azure.Authentication
 {
-    using Cake.Core;
-    using Cake.Core.Annotations;
+    using Core;
+    using Core.Annotations;
     using Microsoft.Rest;
     using Microsoft.Rest.Azure.Authentication;
     using System;
 
-    [CakeAliasCategory("Sample")]
+    [CakeAliasCategory("Deploy.Azure")]
+    [CakeNamespaceImport("Microsoft.Rest")]
     public static class AddinAliases
     {
         private const string WELLKNOWN_CLIENTID = "1950a258-227b-4e31-a9cf-717495945fc2";
@@ -30,6 +31,35 @@ namespace Cake.Deploy.Azure.Authentication
             }
 
             return UserTokenProvider.LoginSilentAsync(WELLKNOWN_CLIENTID, tenantId, loginName, password).GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Login to Azure Resource Manager as active directory application
+        /// </summary>
+        /// <param name="ctx"></param>
+        /// <param name="tenantId">TenantId</param>
+        /// <param name="clientId">Other names: servicePrincipalId in VSTS, Application ID in Azure Portal</param>
+        /// <param name="secret">Other names: servicePrincipalKey in VSTS, Password in Azure Portal</param>
+        /// <returns></returns>
+        [CakeMethodAlias]
+        public static ServiceClientCredentials LoginAzure(this ICakeContext ctx, string tenantId, string clientId, string secret)
+        {
+            if (string.IsNullOrWhiteSpace(tenantId))
+            {
+                throw new ArgumentNullException(nameof(tenantId));
+            }
+
+            if (string.IsNullOrWhiteSpace(clientId))
+            {
+                throw new ArgumentNullException(nameof(clientId));
+            }
+
+            if (string.IsNullOrWhiteSpace(secret))
+            {
+                throw new ArgumentNullException(nameof(secret));
+            }
+
+            return ApplicationTokenProvider.LoginSilentAsync(tenantId, clientId, secret).GetAwaiter().GetResult();
         }
     }
 }
